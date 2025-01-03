ef2f245ac34b247df8b21e875682a4d224cda315
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-03 20:26:16 +0530

    Refactor: Created a Generic Class to Handle 3 Variables and Determine Maximum

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,18 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("UC1 - Test Case 1.1 (Max at 1st position): " + MaximumFinder.findMaximum(30, 20, 10));
-        System.out.println("UC1 - Test Case 1.2 (Max at 2nd position): " + MaximumFinder.findMaximum(20, 30, 10));
-        System.out.println("UC1 - Test Case 1.3 (Max at 3rd position): " + MaximumFinder.findMaximum(10, 20, 30));
+        System.out.println("UC1 - Test Case 1.1 (Max at 1st position): " + new MaximumFinder<>(30, 20, 10).testMaximum());
+        System.out.println("UC1 - Test Case 1.2 (Max at 2nd position): " + new MaximumFinder<>(20, 30, 10).testMaximum());
+        System.out.println("UC1 - Test Case 1.3 (Max at 3rd position): " + new MaximumFinder<>(10, 20, 30).testMaximum());
 
-        System.out.println("UC2 - Test Case 2.1 (Max at 1st position): " + MaximumFinder.findMaximum(30.5f, 20.2f, 10.1f));
-        System.out.println("UC2 - Test Case 2.2 (Max at 2nd position): " + MaximumFinder.findMaximum(20.2f, 30.5f, 10.1f));
-        System.out.println("UC2 - Test Case 2.3 (Max at 3rd position): " + MaximumFinder.findMaximum(10.1f, 20.2f, 30.5f));
+        System.out.println("UC2 - Test Case 2.1 (Max at 1st position): " + new MaximumFinder<>(30.5f, 20.2f, 10.1f).testMaximum());
+        System.out.println("UC2 - Test Case 2.2 (Max at 2nd position): " + new MaximumFinder<>(20.2f, 30.5f, 10.1f).testMaximum());
+        System.out.println("UC2 - Test Case 2.3 (Max at 3rd position): " + new MaximumFinder<>(10.1f, 20.2f, 30.5f).testMaximum());
 
-        System.out.println("UC3 - Test Case 3.1 (Max at 1st position): " + MaximumFinder.findMaximum("Peach", "Apple", "Banana"));
-        System.out.println("UC3 - Test Case 3.2 (Max at 2nd position): " + MaximumFinder.findMaximum("Apple", "Peach", "Banana"));
-        System.out.println("UC3 - Test Case 3.3 (Max at 3rd position): " + MaximumFinder.findMaximum("Apple", "Banana", "Peach"));
+        System.out.println("UC3 - Test Case 3.1 (Max at 1st position): " + new MaximumFinder<>("Peach", "Apple", "Banana").testMaximum());
+        System.out.println("UC3 - Test Case 3.2 (Max at 2nd position): " + new MaximumFinder<>("Apple", "Peach", "Banana").testMaximum());
+        System.out.println("UC3 - Test Case 3.3 (Max at 3rd position): " + new MaximumFinder<>("Apple", "Banana", "Peach").testMaximum());
+
     }
+
 }
--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -1,12 +1,23 @@
-public class MaximumFinder {
+class MaximumFinder<T extends Comparable<T>> {
 
-    public static <T extends Comparable<T>> T  findMaximum(T a , T b, T c){
+    private T a;
+    private T b;
+    private T c;
 
+    public MaximumFinder(T a, T b, T c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public T testMaximum() {
+        return findMaximum(a, b, c);
+    }
+
+    public static <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
         T max = a;
-        if(b.compareTo(max) > 0) max = b;
-        if(c.compareTo(max) > 0) max = c;
-
+        if (b.compareTo(max) > 0) max = b;
+        if (c.compareTo(max) > 0) max = c;
         return max;
-
     }
 }
