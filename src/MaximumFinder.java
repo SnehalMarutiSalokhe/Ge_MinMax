7221b83f2ab316c2a9e94658652eb97d98d9fa0d
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-03 20:11:12 +0530

    refactored: unified maximum finder logic using a generic method supporting Comparable types

--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -1,34 +1,12 @@
 public class MaximumFinder {
-    public static Integer findMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
 
-    public static Float findMaximum(Float a, Float b, Float c) {
-        Float max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
-        return max;
-    }
+    public static <T extends Comparable<T>> T  findMaximum(T a , T b, T c){
 
-    public static String findMaximum(String a, String b, String c) {
-        String max = a;
-        if (b.compareTo(max) > 0) {
-            max = b;
-        }
-        if (c.compareTo(max) > 0) {
-            max = c;
-        }
+        T max = a;
+        if(b.compareTo(max) > 0) max = b;
+        if(c.compareTo(max) > 0) max = c;
+
         return max;
+
     }
 }
