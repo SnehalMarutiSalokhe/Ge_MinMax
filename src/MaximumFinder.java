f849977989df452d725ddb2cdca0df72f1b944cd
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-03 20:39:28 +0530

    Refactored: Extend Max Method to Print Maximum to Std Out Using Generic Method

--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -4,17 +4,22 @@ class MaximumFinder<T extends Comparable<T>> {
 
     private T[] values;
 
-
     public MaximumFinder(T... values) {
         this.values = values;
     }
 
     public T testMaximum() {
-        return findMaximum(values);
+        T max = findMaximum(values);
+        printMax(max);
+        return max;
     }
 
     public static <T extends Comparable<T>> T findMaximum(T... values) {
         Arrays.sort(values);
         return values[values.length - 1];
+    }
+
+    public static <T> void printMax(T max) {
+        System.out.println("Maximum value: " + max);
     }
 }
