421fed0435ea5bd35daf7a485e23b572bcb3ee0b
SnehalMarutiSalokhe <snehal.maruti.salokhe@gmail.com>
2025-01-03 20:34:00 +0530

    Refactor: Extend the Max Method to Handle Multiple Parameters Using Varargs and Sorting

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,10 @@ public class Main {
         System.out.println("UC3 - Test Case 3.2 (Max at 2nd position): " + new MaximumFinder<>("Apple", "Peach", "Banana").testMaximum());
         System.out.println("UC3 - Test Case 3.3 (Max at 3rd position): " + new MaximumFinder<>("Apple", "Banana", "Peach").testMaximum());
 
+        System.out.println("UC4 - Test Case 4.1 (Max from multiple integers): " + new MaximumFinder<>(30, 20, 10, 40, 25).testMaximum());
+        System.out.println("UC4 - Test Case 4.2 (Max from multiple floats): " + new MaximumFinder<>(30.5f, 20.2f, 10.1f, 35.0f).testMaximum());
+        System.out.println("UC4 - Test Case 4.3 (Max from multiple strings): " + new MaximumFinder<>("Peach", "Apple", "Banana", "Mango").testMaximum());
+
     }
 
 }
--- src/MaximumFinder.java
+++ src/MaximumFinder.java
@@ -1,23 +1,20 @@
+import java.util.Arrays;
+
 class MaximumFinder<T extends Comparable<T>> {
 
-    private T a;
-    private T b;
-    private T c;
+    private T[] values;
 
-    public MaximumFinder(T a, T b, T c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+
+    public MaximumFinder(T... values) {
+        this.values = values;
     }
 
     public T testMaximum() {
-        return findMaximum(a, b, c);
+        return findMaximum(values);
     }
 
-    public static <T extends Comparable<T>> T findMaximum(T a, T b, T c) {
-        T max = a;
-        if (b.compareTo(max) > 0) max = b;
-        if (c.compareTo(max) > 0) max = c;
-        return max;
+    public static <T extends Comparable<T>> T findMaximum(T... values) {
+        Arrays.sort(values);
+        return values[values.length - 1];
     }
 }
